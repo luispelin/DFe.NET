cbead1f3b051b491b888d2b93760088957ed5543
Luis Pelin <luispelin@gmail.com>
2019-05-31 14:19:46 -0300

    Backup para nao perder as alteracoes ja feitas

--- NFe.Danfe.Fast/DanfeBase.cs
+++ NFe.Danfe.Fast/DanfeBase.cs
@@ -33,7 +33,7 @@
 
 using System.IO;
 using FastReport;
-using FastReport.Export.Pdf;
+using FastReport.Export.PdfSimple;
 using NFe.Danfe.Base;
 
 namespace NFe.Danfe.Fast
@@ -48,7 +48,8 @@ namespace NFe.Danfe.Fast
         /// <param name="modal">Se true, exibe a visualização em Modal. O modo modal está disponível apenas para WinForms</param>
         public void Visualizar(bool modal = true)
         {
-            Relatorio.Show(modal);
+			// Este método não será útil para o SINFe e é muito complicado de migrar para a nova versão do FastReport
+            //Relatorio.Show(modal);
         }
 
         /// <summary>
@@ -58,35 +59,37 @@ namespace NFe.Danfe.Fast
         /// <param name="modal">Se true, exibe a visualização em Modal. O modo modal está disponível apenas para WinForms</param>
         public void ExibirDesign(bool modal = false)
         {
-            Relatorio.Design(modal);
-        }
+			// Este método não será útil para o SINFe e é muito complicado de migrar para a nova versão do FastReport
+			//Relatorio.Design(modal);
+		}
 
-        /// <summary>
-        /// Envia a impressão do DANFE da NFCe diretamente para a impressora
-        /// </summary>
-        /// <param name="exibirDialogo">Se true exibe o diálogo Imprimindo...</param>
-        /// <param name="impressora">Passe a string com o nome da impressora para imprimir diretamente em determinada impressora. Caso contrário, a impressão será feita na impressora que estiver como padrão</param>
-        public void Imprimir(bool exibirDialogo = true, string impressora = "")
+		/// <summary>
+		/// Envia a impressão do DANFE da NFCe diretamente para a impressora
+		/// </summary>
+		/// <param name="exibirDialogo">Se true exibe o diálogo Imprimindo...</param>
+		/// <param name="impressora">Passe a string com o nome da impressora para imprimir diretamente em determinada impressora. Caso contrário, a impressão será feita na impressora que estiver como padrão</param>
+		public void Imprimir(bool exibirDialogo = true, string impressora = "")
         {
-            Relatorio.PrintSettings.ShowDialog = exibirDialogo;
-            Relatorio.PrintSettings.Printer = impressora;
-            Relatorio.Print();
-        }
+			// Este método não será útil para o SINFe e é muito complicado de migrar para a nova versão do FastReport
+			//Relatorio.PrintSettings.ShowDialog = exibirDialogo;
+			//Relatorio.PrintSettings.Printer = impressora;
+			//Relatorio.Print();
+		}
 
-        /// <summary>
-        /// Converte o DANFE para PDF e salva-o no caminho/arquivo indicado
-        /// </summary>
-        /// <param name="arquivo">Caminho/arquivo onde deve ser salvo o PDF do DANFE</param>
-        public void ExportarPdf(string arquivo)
+		/// <summary>
+		/// Converte o DANFE para PDF e salva-o no caminho/arquivo indicado
+		/// </summary>
+		/// <param name="arquivo">Caminho/arquivo onde deve ser salvo o PDF do DANFE</param>
+		public void ExportarPdf(string arquivo)
         {
             Relatorio.Prepare();
-            Relatorio.Export(new PDFExport(), arquivo);
+            Relatorio.Export(new PDFSimpleExport(), arquivo);
         }
 
         public void ExportarPdf(Stream outputStream)
         {
             Relatorio.Prepare();
-            Relatorio.Export(new PDFExport(), outputStream);
+            Relatorio.Export(new PDFSimpleExport(), outputStream);
             outputStream.Position = 0;
         }
     }
